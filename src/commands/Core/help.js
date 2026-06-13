8d816197c061119088b32f96706c1dafccdaa71a
nobitaplayz110-lab <nobitaplayz110@gmail.com>
2026-06-13 05:44:50 -0700

    Update help.js

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -169,12 +169,12 @@ export async function createInitialHelpMenu(client) {
 
     const supportButton = new ButtonBuilder()
         .setLabel("Support Server")
-        .setURL("https://discord.gg/QnWNz2dKCE")
+        .setURL("https://discord.gg/C5ZTcezp")
         .setStyle(ButtonStyle.Link);
 
     const touchpointButton = new ButtonBuilder()
-        .setLabel("Learn from Touchpoint")
-        .setURL("https://www.youtube.com/@TouchDisc")
+        .setLabel("PLAY MOON MC")
+        .setURL("https://discord.gg/C5ZTcezp")
         .setStyle(ButtonStyle.Link);
 
     const selectRow = createSelectMenu(
